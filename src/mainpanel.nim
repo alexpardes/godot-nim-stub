5f8b82b0cef382604140a31ad1ed16b030236596
Alexander Pardes <alexpardes@gmail.com>
2021-02-18 20:29:21 -0800

    Access directSpaceState

--- src/mainpanel.nim
+++ src/mainpanel.nim
@@ -1,10 +1,10 @@
 # Copyright 2017 Xored Software, Inc.
 
 import godot
-import godotapi / [scene_tree, resource_loader, packed_scene, panel,
-                   global_constants, input_event_mouse_button]
+import godotapi / [scene_tree, resource_loader, packed_scene, spatial,
+                   global_constants, input_event_mouse_button, world, physics_direct_space_state]
 
-gdobj MainPanel of Panel:
+gdobj MainPanel of Spatial:
   method ready*() =
     self.setProcessInput(true)
 
@@ -13,6 +13,4 @@ gdobj MainPanel of Panel:
       let ev = event as InputEventMouseButton
       if ev.buttonIndex == BUTTON_LEFT:
         self.getTree().setInputAsHandled()
-        let scene = load("res://scene.tscn") as PackedScene
-        self.getTree().root.addChild(scene.instance())
-        self.queueFree()
+        let state = self.getWorld().directSpaceState
